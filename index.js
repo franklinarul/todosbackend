d38f62aa145cc53dd4d16bce4b7a274d98304345
franklinarul <franklinarul24@gmail.com>
2025-09-05 18:59:55 +0530

    todo backend created

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express")
+const cors = require("cors")
+const mongose = require("mongoose")
+const app = express()
+
+app.use(cors())
+
+app.use(express.json())
+
+mongose.connect("mongodb://127.0.0.1:27017/todos").then(function(){
+    console.log("database connected....");
+})
+const todos = mongose.model("todos",{name:String},"action")
+
+
+app.listen(5000,function(){
+    console.log("server started....")
+})
+
+app.get("/addtodo",function(req,res){
+    
+    todos.find().then(function(retdata){
+        res.send(retdata)
+    })
+
+})
+app.post("/passvalue",function(req,res){
+    const newtodo = req.body.todovalue
+    const todol = new todos({
+        name:newtodo
+    });
+    todol.save().then(function(){
+        console.log("saved")
+    })
+   res.send(todol);
+})
+
+app.delete("/removevalue/:id",function(req,res){
+  const id = req.params.id
+  todos.findByIdAndDelete(id).then(function(){
+    console.log("deleting the field")
+  });
+})
+
